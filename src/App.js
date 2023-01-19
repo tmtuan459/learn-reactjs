602c488d7f8d7dced9dd9fb95b77bea686307e3f
Dylan <tmtuan459@gmail.com>
2023-01-19 13:16:19 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello Tran Minh Tuan
         </a>
       </header>
     </div>
