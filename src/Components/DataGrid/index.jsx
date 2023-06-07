8b3bb5db2c8676cda91676bec9cd5fe75bc7db60
Dylan <tmtuan459@gmail.com>
2023-06-07 18:59:08 +0700

    final-version2

--- src/Components/DataGrid/index.jsx
+++ src/Components/DataGrid/index.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { DataGrid } from "@mui/x-data-grid";
 
 const columns = [
   { field: "id", headerName: "ID", width: 70 },
@@ -38,13 +37,13 @@ export default function DataTable() {
   return (
     <div style={{ height: 400, width: "100%" }}>
       <p>Danh Sách</p>
-      <DataGrid
+      {/* <DataGrid
         rows={rows}
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
-      />
+      /> */}
     </div>
   );
 }
